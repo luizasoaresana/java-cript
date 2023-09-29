71edb6f161cdd5b83135bf32a7351a3309e0c80e
luizasoaresana <108340502+luizasoaresana@users.noreply.github.com>
2023-09-29 14:21:45 -0300

    28-09-2023

---
+++ java-cript-main/java-cript-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,9 @@
+// Pom-----------------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+   
+
+}
+
+document.querySelectorAll('.tecla')
+
